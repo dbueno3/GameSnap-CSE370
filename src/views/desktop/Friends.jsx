b26d046801188fd2507af569ae0a7b598d50964c
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2023-04-02 15:34:32 -0400

    search field now searches the user and prints it in console #111

--- src/views/desktop/Friends.jsx
+++ src/views/desktop/Friends.jsx
@@ -1,5 +1,48 @@
+import { useState } from "react";
+
 const Friends = () => {
-  return <h1>Friends Page</h1>;
+  const [searchedUser, setSearchedUser] = useState("");
+  return (
+    <div id="friendPageMain">
+      <div id="searchBoxContainer">
+        <input
+          type="text"
+          style={{ width: "50vw" }}
+          placeholder="username of a user"
+          onChange={(e) => {
+            setSearchedUser(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            fetch(
+              process.env.REACT_APP_API_PATH +
+                `/users?attributes=%7B%0A%20%20%22path%22%3A%20%22username%22%2C%0A%20%20%22equals%22%3A%20%22${searchedUser}%22%0A%7D`,
+              {
+                method: "get",
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: "Bearer " + sessionStorage.getItem("token"),
+                },
+              }
+            )
+              .then((res) => res.json())
+              .then((res) => {
+                if (res) {
+                  if (res[0][0] === undefined) {
+                    console.log("user not found");
+                  } else {
+                    console.log(res[0][0]);
+                  }
+                }
+              });
+          }}
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Friends;
