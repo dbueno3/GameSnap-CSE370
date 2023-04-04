943a2bd641ae299e5bccec33389b551dcd7750a7
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2023-04-04 14:46:45 -0400

    created the navbar component #110

--- src/views/desktop/UserProfile.jsx
+++ src/views/desktop/UserProfile.jsx
@@ -2,18 +2,23 @@
 import { useNavigate } from "react-router-dom";
 
 import RenderProfile from "../../Component/RenderProfile.jsx";
+import NavbarOwn from "../../Component/NavbarOwn.jsx";
+
 const UserProfile = () => {
   let navigate = useNavigate();
   return (
-    <RenderProfile userId={sessionStorage.getItem("user")}>
-      <button
-        onClick={() => {
-          navigate("/edit_profile");
-        }}
-      >
-        Edit
-      </button>
-    </RenderProfile>
+    <>
+      <NavbarOwn />
+      <RenderProfile userId={sessionStorage.getItem("user")}>
+        <button
+          onClick={() => {
+            navigate("/edit_profile");
+          }}
+        >
+          Edit
+        </button>
+      </RenderProfile>
+    </>
   );
 };
 
