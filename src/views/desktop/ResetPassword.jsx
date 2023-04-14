4b6b5bf629de4cad75ece7212595e4b4cc786aaf
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2023-04-14 15:56:03 -0400

    reset password #136

--- src/views/desktop/ResetPassword.jsx
+++ src/views/desktop/ResetPassword.jsx
@@ -1,7 +1,13 @@
 //Router
 import { useNavigate } from "react-router-dom";
 
+import { useState } from "react";
+
 const ResetPassword = () => {
+  const [error, setError] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [token, setToken] = useState("");
   let navigate = useNavigate();
   return (
     <>
@@ -36,13 +42,62 @@ const ResetPassword = () => {
       <u>
         <h6>Reset Password</h6>
       </u>
-      <input type="text" placeholder="auth token*" />
+      <input
+        type="text"
+        placeholder="auth token*"
+        onChange={(e) => {
+          setToken(e.target.value);
+        }}
+      />
       <br />
-      <input type="password" placeholder="new password*" />
+      <input
+        type="password"
+        placeholder="new password*"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
       <br />
-      <input type="password" placeholder="confirm password*" />
+      <input
+        type="password"
+        placeholder="confirm password*"
+        onChange={(e) => {
+          setConfirmPassword(e.target.value);
+        }}
+      />
       <br />
-      <button>Reset</button>
+      <p style={{ color: "red", fontSize: "10px" }}>{error}</p>
+      <button
+        onClick={() => {
+          if (password !== confirmPassword) {
+            setError("Passwords don't match");
+          } else {
+            fetch(process.env.REACT_APP_API_PATH + `/auth/reset-password`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + sessionStorage.getItem("token"),
+              },
+              body: JSON.stringify({
+                token: token,
+                password: password,
+              }),
+            })
+              .then((response) => {
+                if (response.status === 200) {
+                  navigate("/");
+                } else {
+                  setError("Something went wrong, please verify you have the right auth token");
+                }
+              })
+              .catch((error) => {
+                alert("An error occurred while sending the token to your email. Please try again later.");
+              });
+          }
+        }}
+      >
+        Reset
+      </button>
     </>
   );
 };
