d1bddd20d7be8dbeb163c293f2d34b12196a206c
Daniel Bueno <Dbueno3@buffalo.edu>
2023-04-24 14:15:24 -0400

    Created Messaging page #141

---
+++ src/views/desktop/Messages.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import react from "react";
+import NavbarOwn from "../../Component/NavbarOwn";
+import { Navigate } from "react-router-dom";
+
+const Icon = (event) => { 
+
+}
+
