74cfa9b71024fcdfb22b792bfffeb1a6a7270e19
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2023-04-25 00:34:59 -0400

    frontend with sameple message structure #144

--- src/views/desktop/Chat.jsx
+++ src/views/desktop/Chat.jsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import NavbarOwn from "../../Component/NavbarOwn";
 
@@ -7,20 +8,69 @@ import "../style.css";
 const Chat = () => {
   const params = useParams();
   const connId = params.connId;
+  const [messages, setMessages] = useState([
+    {
+      id: 1,
+      from: "John",
+      to: "Mary",
+      message: "Hello Mary, how are you?",
+      timestamp: "2022-04-25T12:34:56Z",
+    },
+    {
+      id: 2,
+      from: "Mary",
+      to: "John",
+      message: "Hi John, I'm good, thanks! How about you?",
+      timestamp: "2022-04-25T12:35:23Z",
+    },
+    {
+      id: 3,
+      from: "John",
+      to: "Mary",
+      message: "I'm doing well, thanks!",
+      timestamp: "2022-04-25T12:36:12Z",
+    },
+  ]);
+
+  //Get the connection
+  useEffect(() => {
+    fetch(process.env.REACT_APP_API_PATH + `/connections/${connId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + sessionStorage.getItem("token"),
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+  }, []);
+
   return (
     <>
       <NavbarOwn />
-      <h1>{connId}</h1>
-      <div className="chatRoom">
-        <input
-          style={{ width: "90%", bottom: 0, left: 0 }}
-          className="chatInput"
-          type="text"
-          placeholder="Say something nice"
-        />
-        <button style={{ width: "8%", bottom: 0, right: 0 }} className="chatInput">
-          Send
-        </button>
+      <div class="chatRoom">
+        <div>
+          {messages.map((msg) => (
+            <div key={msg.id}>
+              <p>
+                {msg.from}: {msg.message}
+              </p>
+            </div>
+          ))}
+        </div>
+        <div className="chatInputContainer">
+          <input
+            style={{ width: "90%", bottom: 0, left: 0 }}
+            className="chatInput"
+            type="text"
+            placeholder="Say something nice"
+          />
+          <button style={{ width: "8%", bottom: 0, right: 0 }} className="chatInput">
+            Send
+          </button>
+        </div>
       </div>
     </>
   );
