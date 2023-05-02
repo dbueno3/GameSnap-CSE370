332799d9a223747e972b91bbbaf13605ea0ff0ea
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2023-05-02 14:02:05 -0400

    made the profile picture circular #165

--- src/Component/NavbarOwn.jsx
+++ src/Component/NavbarOwn.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import { BsFillPlusCircleFill } from "react-icons/bs";
 import { FaUserFriends } from "react-icons/fa";
@@ -10,10 +10,10 @@ import { MdHome } from "react-icons/md";
 import { CiUser } from "react-icons/ci";
 import { BsFillChatFill } from "react-icons/bs";
 
-import homeIcon from '../assets/home.svg';
-import searchIcon from '../assets/search.svg';
-import addIcon from '../assets/add.svg';
-import messageIcon from '../assets/message.svg';
+import homeIcon from "../assets/home.svg";
+import searchIcon from "../assets/search.svg";
+import addIcon from "../assets/add.svg";
+import messageIcon from "../assets/message.svg";
 
 const NavbarOwn = () => {
   let navigate = useNavigate();
@@ -39,7 +39,7 @@ const NavbarOwn = () => {
     <nav class="navbar">
       {/* <table className="navbartable">
         <tr> */}
-          {/* <td style={{ textAlign: "center", verticalAlign: "middle", height: "5px", paddingRight: "10px" }}>
+      {/* <td style={{ textAlign: "center", verticalAlign: "middle", height: "5px", paddingRight: "10px" }}>
             <CiUser
               className="navbarIcon"
               onClick={() => {
@@ -47,58 +47,40 @@ const NavbarOwn = () => {
               }}
             />
           </td> */}
-          <Link to="/home" className="navbar-item">
-            <img
-              className="bottombar__icon"
-              src={homeIcon || 'https://via.placeholder.com/20'}
-              alt="Home"
-            />
-            <span>Home</span>
-          </Link>
-          <Link to="/explore" className="navbar-item">
-            <img
-              className="bottombar__icon"
-              src={searchIcon || 'https://via.placeholder.com/20'}
-              alt="explore"
-            />
-            <span>Explore</span>
-          </Link>
-
-          <Link to="/friends" className="navbar-item">
-            <FaUserFriends
-              color={"black"}
-              className="bottombar__icon"/>
-            <span>Friends</span>
-          </Link>
-
-          <Link to="/create_post" className="navbar-item">
-              <img
-                className="bottombar__icon"
-                src={addIcon || 'https://via.placeholder.com/20'}
-                alt="Create Post"
-              />
-              <span>Post</span>
-          </Link>
-
-          <Link to="/chat" className="navbar-item">
-          <img
-            className="bottombar__icon"
-            src={messageIcon || 'https://via.placeholder.com/20'}
-            alt="Chat"
-          />
-          <span>Chat</span>
+      <Link to="/home" className="navbar-item">
+        <img className="bottombar__icon" src={homeIcon || "https://via.placeholder.com/20"} alt="Home" />
+        <span className="navbarText">Home</span>
       </Link>
-<Link to="/profile" className="navbar-item">
-            <img
-              src={proPic}
-              className="bottombar__icon"
-              alt="profile"
-              style={{ cursor: "pointer", verticalAlign: "middle" }}
-              onClick={() => {
-                navigate("/edit_profile_mobile");
-              }}
-            />
-            <span>Profile</span>
+      <Link to="/explore" className="navbar-item">
+        <img className="bottombar__icon" src={searchIcon || "https://via.placeholder.com/20"} alt="explore" />
+        <span className="navbarText">Explore</span>
+      </Link>
+
+      <Link to="/friends" className="navbar-item">
+        <FaUserFriends color={"black"} className="bottombar__icon" />
+        <span className="navbarText">Friends</span>
+      </Link>
+
+      <Link to="/create_post" className="navbar-item">
+        <img className="bottombar__icon" src={addIcon || "https://via.placeholder.com/20"} alt="Create Post" />
+        <span className="navbarText">Post</span>
+      </Link>
+
+      <Link to="/chat" className="navbar-item">
+        <img className="bottombar__icon" src={messageIcon || "https://via.placeholder.com/20"} alt="Chat" />
+        <span className="navbarText">Chat</span>
+      </Link>
+      <Link to="/profile" className="navbar-item">
+        <img
+          src={proPic}
+          className="bottombar__icon bottombar_profile"
+          alt="profile"
+          style={{ cursor: "pointer", verticalAlign: "middle" }}
+          onClick={() => {
+            navigate("/edit_profile_mobile");
+          }}
+        />
+        <span className="navbarText">Profile</span>
       </Link>
     </nav>
   );
